0f4f51bdb328c7a84c0495e998942f343a85cbb7
Dicer-TDP <51524891+Dicer-TDP@users.noreply.github.com>
2020-11-06 22:11:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["link project"]
+const urls = ["https://glitch.com/edit/#!/main-proje?path=server.js%3A29%3A0"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
